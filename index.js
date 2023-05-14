eb131221797df5a5b339df7424b8eecd57d2e123
Wasim Akram <90859273+Wasiim12345@users.noreply.github.com>
2023-05-14 23:58:29 +0530

    portfolio complete updated

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 const year = document.getElementById('year');
 year.innerHTML = new Date().toDateString();
 
-// CODE FOR THE PROJECT SECTION TO CALL DYNAMIC
+// OBJECT STARTS FOR THE PROJECT SECTION TO CALL DYNAMIC
 const projectElement = document.getElementById('total-project');
 
 const project = [
+    // HTML-CSS PROJECT STARTS HERE
     {
         name: 'Food Web using HTML-CSS',
         image: 'foods-web.png',
@@ -29,7 +30,11 @@ const project = [
         image: 'html-css-project.png',
         githubURL: 'https://github.com/Wasiim12345/nuuk-project/blob/main/index.html',
         liveURL: 'https://wasiim12345.github.io/nuuk-project/'
-    }, {
+    }, 
+    // HTML-CSS PROJECTS ENDS HERE
+
+    // JAVASCRIPT PROJECTS STARTS HERE
+    {
         name: 'Calculator using JavaScript',
         image: 'calculator-js.png',
         githubURL: 'https://github.com/Wasiim12345/calculator/blob/main/Calculator.html',
@@ -40,12 +45,56 @@ const project = [
         image: 'quiz-app-js.png',
         githubURL: 'https://github.com/Wasiim12345/quiz-app/blob/main/index.html',
         liveURL: 'https://wasiim12345.github.io/quiz-app/'
+    },
+    {
+        name: 'Grid JS Assignment',
+        image: 'html-grid-js.png',
+        githubURL: 'https://github.com/Wasiim12345/assign.-js-html-grid/blob/main/index.html',
+        liveURL: 'https://wasiim12345.github.io/assign.-js-html-grid/'
+    },
+    {
+        name: 'Event JS Assignment',
+        image: 'events-js.png',
+        githubURL: 'https://github.com/Wasiim12345/javascript-event/blob/main/index.html',
+        liveURL: 'https://wasiim12345.github.io/javascript-event/'
+    }
+    // JAVASCRIPT PROJECTS ENDS HERE
+
+]
+// OBJECT ENDS FOR THE PROJECT SECTION
+
+// OBJECT STARTS FOR THE SKILLS SECTCTION TO CALL DYNAMIC
+const skillsElement = document.getElementById('skill');
+
+const skills = [
+    {
+        name: 'HTML5',
+        percentage: 95
+    },
+    {
+        name: 'CSS3',
+        percentage: 95
+    },
+    {
+        name: 'JavaScript',
+        percentage: 90
+    },
+    {
+        name: 'React.js',
+        percentage: 80
+    },
+    {
+        name: 'Bootstrap',
+        percentage: 100
     }
 
 ]
+// OBJECT ENDS FOR THE SKILLS SECTCTION TO CALL DYNAMIC
 
+
+// JAVASCRIPT CODE FOR PROJECT SECTION TO CALL DYNAMIC
 let output = '';
-for (let i = 0; i < project.length; i++) {
+for (let i = 0; i < project.length; i++) { 
     output += `
     <div class="project">
         <img src="images/${project[i].image}" alt="Food Web using HTML/CSS">
@@ -59,4 +108,22 @@ for (let i = 0; i < project.length; i++) {
     </div>`
 }
 
+// SKILLS STRING LITERAL PART STARTS HERE
+let result = '';
+let skillRemaining;
+for(let i = 0; i < skills.length; i++) {
+    skillRemaining = 100 - skills[i].percentage;
+    result += `
+    <div class="skill-name">
+                <div class="tech-name">${skills[i].name}</div>
+                <div class="tech-skill">
+                    <div class="skill-show" style="width: ${skills[i].percentage}%;"></div>
+                    <div class="skills-per" style="width: ${skillRemaining}%;">${skills[i].percentage}%</div>
+                </div>
+            </div>`
+}
+// SKILLS STRING LITERAL PART ENDS HERE
+
+
 projectElement.innerHTML = output;
+skillsElement.innerHTML = result;
