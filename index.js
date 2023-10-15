bddd166c9c712a09bd42b82bced494042cd83ec9
wasim <iamwasim.akram04@gmail.com>
2023-10-15 22:11:19 +0530

    typeWriter added

--- index.js
+++ index.js
@@ -15,6 +15,52 @@ btn.addEventListener('click', () => {
 const year = document.getElementById('year');
 year.innerHTML = new Date().toDateString();
 
+// TYPEWRITER CODES START FROM HERE
+var typeWriterElement = document.getElementById('typewriter');
+
+var textArray = ["a human being", "A Software Engineer", "Coder & writes beautiful codes"];
+
+function delWriter(text, i, cb) {
+    if (i >= 0) {
+        typeWriterElement.innerHTML = text.substring(0, i--);
+        var rndBack = 10 + Math.random() * 100;
+        setTimeout(function () {
+            delWriter(text, i, cb);
+        }, rndBack);
+    } else if (typeof cb == 'function') {
+        setTimeout(cb, 1000);
+    }
+};
+
+function typeWriter(text, i, cb) {
+    if (i < text.length + 1) {
+        typeWriterElement.innerHTML = text.substring(0, i++);
+        var rndTyping = 250 - Math.random() * 100;
+        setTimeout(function () {
+            typeWriter(text, i++, cb)
+        }, rndTyping);
+    } else if (i === text.length + 1) {
+        setTimeout(function () {
+            delWriter(text, i, cb)
+        }, 1000);
+    }
+};
+
+function StartWriter(i) {
+    if (typeof textArray[i] == "undefined") {
+        setTimeout(function () {
+            StartWriter(0)
+        }, 1000);
+    } else if (i < textArray[i].length + 1) {
+        typeWriter(textArray[i], 0, function () {
+            StartWriter(i + 1);
+        });
+    }
+};
+setTimeout(function () {
+    StartWriter(0);
+}, 1000);
+
 // OBJECT STARTS FOR THE PROJECT SECTION TO CALL DYNAMIC
 const projectElement = document.getElementById('total-project');
 
